e9456a3e61db8fe03237b7ebf43bebb5b9e06fe4
sharif-hosen <mdsharifkhan762@gmail.com>
2024-05-19 08:13:02 +0600

    shortcuts keys

---
+++ file.php
@@ -0,0 +1,35 @@
+<?php 
+    
+    Ctrl+Shift+P, or F1	Show Command Palette
+    Ctrl+Shift+N	New window/instance
+    Ctrl+Shift+W	Close window/instance
+    Ctrl+Shift+K	Delete line
+
+    Ctrl+K Ctrl+[	Fold (collapse) all subregions
+    Ctrl+K Ctrl+]	Unfold (uncollapse) all subregions
+
+    Ctrl+Enter	Insert line below
+    Ctrl+Shift+Enter	Insert line above
+
+
+    Ctrl+Shift+\	Jump to matching bracket
+
+    Ctrl+P	Quick Open, Go to File…
+    Ctrl+X	Cut line (empty selection)
+    Ctrl+C	Copy line (empty selection)
+
+    Ctrl+G	 Line serach 
+
+    Ctrl+F	Find
+    Ctrl+H	Replace
+
+    Ctrl+/	Toggle line comment
+    Shift+Alt+A	Toggle block comment
+
+    Ctrl+Home	Go to beginning of file
+    Ctrl+End	Go to end of file
+
+    Alt+ ↑ / ↓	Move line up/down
+    Shift+Alt + ↓ / ↑	Copy line up/down
+
+  
